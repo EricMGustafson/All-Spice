dc631e5cca0f461d6b29ff15ad3e6bcfd6aa2f82
EricMGustafson <emgustafson22@gmail.com>
2022-05-26 15:08:57 -0600

    init commit

---
+++ checkpoint8/Models/Recipe.cs
@@ -0,0 +1,12 @@
+namespace checkpoint8.Models
+{
+  public class Recipe
+  {
+    public int id { get; set; }
+    public string picture { get; set; }
+    public string title { get; set; }
+    public string subTitle { get; set; }
+    public string category { get; set; }
+    public string creatorId { get; set; }
+  }
+}
