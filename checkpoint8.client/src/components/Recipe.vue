b7e3ab79e09efd156227384ca0572aeebc8cb25e
EricMGustafson <emgustafson22@gmail.com>
2022-05-29 02:39:05 -0600

    home page styled

---
+++ checkpoint8.client/src/components/Recipe.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="col-md-4 mt-5 d-flex justify-content-center">
+    <div :style="{ backgroundImage: `url(${recipe.picture})`}" class="recipe-card rounded shadow">
+      <div class="h-100 d-flex flex-column justify-content-between">
+        <div class="d-flex justify-content-between m-3">
+          <div class=" rounded-pill bg-info pt-25">
+            <h3 class="px-3 mb-0">{{recipe.category}}</h3>
+          </div>
+          <div class="bg-info rounded-pill px-2">
+            <i v-if="favorite" class="mdi mdi-star mdi-48px text-danger"></i>
+            <i v-else class="mdi mdi-star-outline mdi-48px text-danger"></i>
+          </div>
+        </div>
+        <div class="rounded-pill bg-info m-3">
+            <h2 class="ms-4">{{recipe.title}}</h2>
+            <h3 class="ms-4">{{recipe.subTitle}}</h3>  
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    recipe: {
+      type: Object,
+      required: true, 
+    }
+  },
+  setup(){
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.recipe-card {
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 40vh;
+  width: 90%;
+  }
+.pt-25{
+  padding-top: 20px;
+}
+</style>
--- checkpoint8.client/src/pages/HomePage.vue
+++ checkpoint8.client/src/pages/HomePage.vue
@@ -1,36 +1,89 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo" class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
+<div class="row justify-content-center">
+  <div class="col-md-12 intro rounded shadow-lg position-relative">
+    <div class="pt-2 d-flex justify-content-between">
+      <div>
+        <Login />
+      </div>
+      <div>
+        <form submit="" class="d-flex justify-content-end">
+          <div class="input-group mb-3 ">
+            <input type="text" class="form-control" placeholder="Search..." aria-label="Search..." aria-describedby="button-addon2">
+            <button class="btn btn-outline-secondary bg-light" type="submit" id="button-addon2"><i class="mdi mdi-magnify"></i></button>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div class="intro-text text-light d-flex flex-column justify-content-center align-items-center">
+      <h1 class="f-28">All-Spice</h1>
+      <p class="f-16">Cherish Your Family</p>
+      <p class="f-16">And Their Cooking</p>
+    </div>
+    <div class="position-absolute filter-bar bg-light shadow rounded">
+      <div class="d-flex justify-content-around h-100 align-items-center">
+        <h4>All</h4>
+        <h4>My Recipes</h4>
+        <h4>Favorites</h4>
+      </div>
     </div>
   </div>
+</div>
+<div class="row mt-6 flex-wrap scrollable-y">
+  <Recipe v-for="r in recipes" :key="r.id" :recipe="r"/>
+</div>
 </template>
 
 <script>
+import { computed, onMounted } from '@vue/runtime-core'
+import { logger } from '../utils/Logger'
+import Pop from '../utils/Pop'
+import { recipesService } from '../services/RecipesService'
+import { AppState } from '../AppState'
 export default {
-  name: 'Home'
+  name: 'Home',
+  setup() {
+    onMounted(async () => {
+      try {
+        await recipesService.getAllRecipes()
+      } catch (error) {
+        logger.error(error)
+        Pop.toast(error.message, 'error')
+      }
+    })
+    return {
+      recipes: computed(() => AppState.recipes)
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
-.home{
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-  .home-card{
-    width: 50vw;
-    > img{
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
+.intro {
+  background-image: url('https://static.onecms.io/wp-content/uploads/sites/9/2020/06/29/creative-pandemic-cooking-essay-FT-BLOG0620.jpg');
+  object-fit: center;
+  height: 35vh;
+  width: 97vw;
+  margin-top: 2.5vh;
+}
+.intro-text{
+  height: 25vh;
+}
+h1{
+  text-shadow: 4px 4px #0e0e0e;
+}
+p{
+  margin-bottom: 0rem !important;
+  text-shadow: 2.5px 2.5px #0e0e0e;
+}
+.filter-bar {
+  width: 33%;
+  height: 8vh;
+  right: 33%;
+}
+.mt-6 {
+  margin-top: 4em;
+}
+::-webkit-scrollbar{
+  width: 1px;
 }
 </style>
